dee64e745341296c514bc4f5eb6652cb54af9318
Oren <orengolan@gmail.com>
2015-05-31 11:39:25 -0700

    shellcheck errors

--- bin/replace-client-id.sh
+++ bin/replace-client-id.sh
@@ -5,11 +5,11 @@ host=$2
 port=$3
 
 if [ "$#" -eq 3 ]; then
-  sed s,CLIENT_ID,$client_id,g -i app/rp.html app/scripts/app.js
-  sed s,APP_HOST,$host,g -i app/rp.html app/scripts/app.js
-  sed s,APP_PORT,$port,g -i app/rp.html app/scripts/app.js
-  sed s,APP_HOST,$host,g -i Gruntfile.js
-  sed s,APP_PORT,$port,g -i Gruntfile.js
+  sed s,CLIENT_ID,"$client_id",g -i app/rp.html app/scripts/app.js
+  sed s,APP_HOST,"$host",g -i app/rp.html app/scripts/app.js
+  sed s,APP_PORT,"$port",g -i app/rp.html app/scripts/app.js
+  sed s,APP_HOST,"$host",g -i Gruntfile.js
+  sed s,APP_PORT,"$port",g -i Gruntfile.js
 else
   echo Please pass client id, host, and port as arguments. for example: bin/replace-client-id.sh 6d327f50-0aa7-4b0a-9bab-b558d9027e27 http://my-server.com 9000
 fi
