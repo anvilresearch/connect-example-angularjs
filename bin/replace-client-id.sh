5049aa765b313d5e271048e97fe64923974351e1
Oren <orengolan@gmail.com>
2015-05-31 11:42:00 -0700

    hardcode http because of grunt

--- bin/replace-client-id.sh
+++ bin/replace-client-id.sh
@@ -11,6 +11,6 @@ if [ "$#" -eq 3 ]; then
   sed s,APP_HOST,"$host",g -i Gruntfile.js
   sed s,APP_PORT,"$port",g -i Gruntfile.js
 else
-  echo Please pass client id, host, and port as arguments. for example: bin/replace-client-id.sh 6d327f50-0aa7-4b0a-9bab-b558d9027e27 http://my-server.com 9000
+  echo Please pass client id, host, and port as arguments. for example: bin/replace-client-id.sh 6d327f50-0aa7-4b0a-9bab-b558d9027e27 my-server.com 9000
 fi
 
