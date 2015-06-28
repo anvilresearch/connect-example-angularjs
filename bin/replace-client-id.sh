665c1cb807ede1e07d6887cbc7e06a0f02d292c0
Christian Smith <smith@anvil.io>
2015-06-28 02:10:30 -0700

    server_host

--- bin/replace-client-id.sh
+++ bin/replace-client-id.sh
@@ -3,6 +3,7 @@
 client_id=$1
 host=$2
 port=$3
+server_host=$4
 
 if [ "$#" -eq 3 ]; then
   sed s,CLIENT_ID,"$client_id",g -i app/rp.html app/scripts/app.js
@@ -10,6 +11,7 @@ if [ "$#" -eq 3 ]; then
   sed s,APP_PORT,"$port",g -i app/rp.html app/scripts/app.js
   sed s,APP_HOST,"$host",g -i Gruntfile.js
   sed s,APP_PORT,"$port",g -i Gruntfile.js
+  sed s,SERVER_HOST,"$server_host",g -i app/rp.html app/scripts/app.js
 else
   echo Please pass client id, host, and port as arguments. for example: bin/replace-client-id.sh 6d327f50-0aa7-4b0a-9bab-b558d9027e27 my-server.com 9000
 fi
