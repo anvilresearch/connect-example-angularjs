f805e297443aedec28f1123bd8e9dd98efddcdfb
Christian Smith <smith@anvil.io>
2015-06-28 02:12:26 -0700

    oxford commad. because Vartran

--- bin/replace-client-id.sh
+++ bin/replace-client-id.sh
@@ -13,6 +13,6 @@ if [ "$#" -eq 3 ]; then
   sed s,APP_PORT,"$port",g -i Gruntfile.js
   sed s,SERVER_HOST,"$server_host",g -i app/rp.html app/scripts/app.js
 else
-  echo Please pass client id, host, and port as arguments. for example: bin/replace-client-id.sh 6d327f50-0aa7-4b0a-9bab-b558d9027e27 my-server.com 9000
+  echo Please pass client id, host, port, and server host as arguments. for example: bin/replace-client-id.sh 6d327f50-0aa7-4b0a-9bab-b558d9027e27 my-server.com 9000 https://connect.anvil.io
 fi
 
